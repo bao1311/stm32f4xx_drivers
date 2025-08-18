78e94115881d9ed5b3ce4437fc90b66c3bf36ae9
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-17 22:41:36 -0700

    Find the equivalent of pin to line MISO, MOSI, SCK, NSS of SPI2 in datasheet and the alternate function

--- Src/006spi_tx_testing.c
+++ Src/006spi_tx_testing.c
@@ -5,6 +5,19 @@
  *      Author: gphi1
  */
 #include <stdio.h>
+
+/*
+ * Requirements: Print hello world string through SPI2 peripheral
+ */
+
+/*
+ * Alternate function:
+ * PB12 		-> SPI2_NSS
+ * PB13			-> SPI2_SCK
+ * PB14			-> SPI2_MISO
+ * PB15			-> SPI2_MOSI
+ * Alternate function mode 5
+ */
 int main(void)
 {
 	printf("Hello world");
