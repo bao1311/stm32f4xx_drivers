dec78c34ee2d1ae961934a98710357e92d9aa206
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-25 00:39:59 -0700

    Define NVIC_IRQ_PRI macros

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -22,6 +22,26 @@
  * ARM Cortex M4 Address Section
  */
 
+/*
+ * Define NVIC Priority level
+ */
+#define NVIC_IRQ_PRI0				0
+#define NVIC_IRQ_PRI1				1
+#define NVIC_IRQ_PRI2				2
+#define NVIC_IRQ_PRI3				3
+#define NVIC_IRQ_PRI4				4
+#define NVIC_IRQ_PRI5				5
+#define NVIC_IRQ_PRI6				6
+#define NVIC_IRQ_PRI7				7
+#define NVIC_IRQ_PRI8				8
+#define NVIC_IRQ_PRI9				9
+#define NVIC_IRQ_PRI10				10
+#define NVIC_IRQ_PRI11				11
+#define NVIC_IRQ_PRI12				12
+#define NVIC_IRQ_PRI13				13
+#define NVIC_IRQ_PRI14				14
+#define NVIC_IRQ_PRI15				15
+
 // NVIC ISER(Interrupt Set-enable Register) address
 #define NVIC_ISER0		(__vo uint32_t*)0xE000E100
 #define NVIC_ISER1		(__vo uint32_t*)0xE000E104
