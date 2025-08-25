b64fbcc4ed129523ee2dbd8f9ef9a3041f12b4bc
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-25 00:36:11 -0700

    Change from GPIO_IRQPriorityHandling->GPIO_IRQPriorityConfig

--- drivers/Inc/stm32f407xx_gpio_driver.h
+++ drivers/Inc/stm32f407xx_gpio_driver.h
@@ -161,7 +161,7 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t* pGPIOx, uint8_t PinNumber);
 /*
  * IRQ Configuration and ISR handling
  */
-void GPIO_IRQPriorityHandling(uint8_t IRQNumber, uint8_t IRQPriority);
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
 void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
 void GPIO_IRQHandling(uint8_t PinNumber);
 
