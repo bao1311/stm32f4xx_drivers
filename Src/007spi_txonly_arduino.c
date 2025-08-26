448e7415227b6ba521edf82b9b1ef0fd819f8d9a
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-26 01:49:48 -0700

    Added check to prevent interrupt SPI2 while the communication is ongoing

--- Src/007spi_txonly_arduino.c
+++ Src/007spi_txonly_arduino.c
@@ -115,6 +115,7 @@ int main(void)
 		while (!GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0));
 		delay();
 		SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+		while (SPI_GetFlagStatus(SPI2, SPI_BUSY_FLAG));
 		SPI_PeripheralControl(SPI2, DISABLE);
 	}
 
