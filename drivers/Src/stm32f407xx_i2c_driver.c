4ad3b65fea43ac873233f98f811cb89cb5f31f19
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-29 03:38:08 -0700

    feat(i2c): Implemented I2C_PeripheralControl

--- drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -161,7 +161,17 @@ void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
 /*
  * Other Peripheral Control APIs
  */
-void I2C_PeripheralControl(I2C_RegDef_t* pI2Cx, uint8_t EnorDi);
+void I2C_PeripheralControl(I2C_RegDef_t* pI2Cx, uint8_t EnorDi)
+{
+	if (EnorDi == ENABLE)
+	{
+		pI2Cx->CR1 |= (1 << I2C_CR1_PE);
+	}
+	else
+	{
+		pI2Cx->CR1 &= ~(1 << I2C_CR1_PE);
+	}
+}
 uint8_t I2C_GetFlagStatus(I2C_RegDef_t* pI2Cx, uint8_t FlagName);
 /*
  * Application callback
