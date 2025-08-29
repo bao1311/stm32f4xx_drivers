66c6edeaca697576637d5a2c1f9da35843df4491
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-29 03:15:20 -0700

    Implemented I2C_DeInit

--- drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -1,8 +1,26 @@
 /*
  * stm32f407xx_i2c_driver.c
  *
- *  Created on: Aug 28, 2025
+ *  Created on: Aug 29, 2025
  *      Author: gphi1
  */
 
+#include "stm32f407xx_i2c_driver.h"
 
+
+
+void I2C_DeInit(I2C_RegDef_t* pI2Cx)
+{
+	if (pI2Cx == I2C1)
+	{
+		I2C1_PCLK_DI();
+	}
+	else if (pI2Cx == I2C2)
+	{
+		I2C2_PCLK_DI();
+	}
+	else if (pI2Cx == I2C3)
+	{
+		I2C3_PCLK_DI();
+	}
+}
