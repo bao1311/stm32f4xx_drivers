5289186c1a5fa532be79ba104d4b5fe3e180b1a6
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-08-28 22:50:43 -0700

    Created i2c header and source files

---
+++ drivers/Inc/stm32f407xx_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f407xx_i2c_driver.h
+ *
+ *  Created on: Aug 28, 2025
+ *      Author: gphi1
+ */
+
+#ifndef INC_STM32F407XX_I2C_DRIVER_H_
+#define INC_STM32F407XX_I2C_DRIVER_H_
+
+
+
+#endif /* INC_STM32F407XX_I2C_DRIVER_H_ */
