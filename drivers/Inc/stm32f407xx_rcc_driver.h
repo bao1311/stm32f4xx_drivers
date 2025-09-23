cb63fd2b4df675a8c7c9a8cd0119246c0bd4bc88
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-09-23 03:25:47 -0700

    fea(rcc): Added RCC function header

--- drivers/Inc/stm32f407xx_rcc_driver.h
+++ drivers/Inc/stm32f407xx_rcc_driver.h
@@ -1,7 +1,8 @@
 #ifndef STM32F407XX_RCC_DRIVER_H
 #define STM32F407XX_RCC_DRIVER_H
-
+#include "stm32f407xx.h"
 uint32_t RCC_GetPCLK1Value();
 uint32_t RCC_GetPCLK2Value();
+uint32_t RCC_GetPLLOutputClk();
 
 #endif
